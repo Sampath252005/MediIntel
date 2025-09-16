d7ae344e484ee73b92925be3ef680b63e21efaa3
Sampath252005 <sampathnaik65@gmail.com>
2025-09-16 16:27:10 +0530

    imported tailwind CSS to frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <div className="text-3xl font-bold underline bg-blue-500 ">
+      hello world
+    </div>
+    </>
+  );
+}
