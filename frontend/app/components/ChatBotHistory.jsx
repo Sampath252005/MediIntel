fbe1ac4cfccd47feda702bb1c6587f6b89f8cc59
Sushanth105 <veenaganiga96@gmail.com>
2025-10-11 06:20:16 +0530

    edited

--- frontend/app/components/ChatBotHistory.jsx
+++ frontend/app/components/ChatBotHistory.jsx
@@ -13,7 +13,10 @@ const ChatBotHistory = ({ toggleShowHistory, showHistory}) => {
   const fetchHistory = async () => {
     try {
       const res = await fetch(`http://localhost:8000/chatHistory/1`);
-      if (!res.ok) throw new Error("Failed to fetch chat history");
+      if (!res.ok){
+        console.log("Failed to fetch chat history");
+        return
+      } 
       const data = await res.json();
       setHistory(data.data);
     } catch (err) {
